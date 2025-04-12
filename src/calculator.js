a082e913ce64a09d00068080833c9dbcc0509d5f
APPLE <apple@APPLEs-MacBook-Air.local>
2025-04-12 15:25:41 +0530

    handle additional cases like custom delimiter and negative numbers handling

--- src/calculator.js
+++ src/calculator.js
@@ -3,10 +3,23 @@ function add(numbers) {
 
   let delimiter = /,|\n/;
 
+  if (numbers.startsWith('//')) {
+    const match = numbers.match(/^\/\/(.+)\n/);
+    if (match) {
+      delimiter = new RegExp(match[1]);
+      numbers = numbers.slice(match[0].length);
+    }
+  }
+
   const numberArray = numbers
     .split(delimiter)
     .map(n => parseInt(n, 10))
     .filter(n => !isNaN(n));
+
+  const negatives = numberArray.filter(n => n < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed: ${negatives.join(', ')}`);
+  }
 
   return numberArray.reduce((sum, num) => sum + num, 0);
 }
